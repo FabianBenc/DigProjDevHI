2639030d229b88ae417e309a2313e8e33c535138
Fabian Benc <bencfabian@hotmail.com>
2023-04-13 13:34:51 +0200

    first commit

---
+++ blockaid/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, CreateView  # new
+from django.urls import reverse_lazy  # new
+from .forms import PostForm  # new
+from .models import Post
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class CreatePostView(CreateView):  # new
+    model = Post
+    form_class = PostForm
+    template_name = "post.html"
+    success_url = reverse_lazy("home")
