ff0fed2e9e44b5043a1c40bf0f851f5c63bc18ad
FabianBenc <bencfabian@hotmail.com>
2023-07-14 22:51:20 +0200

    trying to fix allowed hostspt2

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     'https://www.blockaid.azurewebsites.net/',
     'http://www.blockaid.azurewebsites.net/',
     '127.0.0.1',
-    'localhost'
+    'localhost',
 ]
                  
 
